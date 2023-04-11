55814957cc0c453c48e768c3b368a1fb12136d28
Rainer Simon <rainer@rainersimon.io>
2023-04-11 18:58:16 +0200

    Polygon drawing

--- src/index.js
+++ src/index.js
@@ -12,4 +12,37 @@ const canvas = createCanvas(WIDTH, HEIGHT);
 
 const ctx = canvas.getContext('2d');
 
-console.log(annotations);
+ctx.fillStyle = '#000000';
+ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+annotations.forEach(annotation => {
+  const selector = annotation.target.selector[0];
+
+  if (selector.type === 'SvgSelector') {
+    const { value } = selector;
+
+    const [a, b, polygon] = value.match(/(<polygon points=")([^"]*)/) || [];
+    if (polygon) {
+      const [start, ...points] = polygon.split(' ').map(xy => xy.split(',').map(parseFloat));      
+      
+      ctx.fillStyle = '#ff0000';
+      ctx.beginPath();
+      ctx.moveTo(start[0], start[1]);
+
+      points.forEach(([x, y]) => ctx.lineTo(x, y));
+
+      ctx.closePath();
+      ctx.fill();
+    }
+  } else {
+
+  }
+});
+
+console.log('Creating PNG file...');
+const stream = canvas.createPNGStream();
+
+const out = fs.createWriteStream('./3g1xhmd41yfunw.png')
+stream.pipe(out)
+
+out.on('finish', () => console.log('Done.'));
